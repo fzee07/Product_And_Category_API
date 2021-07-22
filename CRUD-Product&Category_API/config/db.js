aa196546a337e856ed25d58ef08e7aa1553f16bb
Faizan Hassan <hassanfaizan1994@gmail.com>
2021-07-22 18:57:58 +0530

    Update db.js

--- CRUD-Product&Category_API/config/db.js
+++ CRUD-Product&Category_API/config/db.js
@@ -1,25 +1,28 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
+module.exports = {
+    connectDB: async() => {
+        //Database Connection
+        mongoose.connect(process.env.MONGO_DB, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useFindAndModify: true
+        });
+        const db = mongoose.connection;
+
+        db.on('error', console.error.bind(console, 'Connection Error:'));
+
+        db.once('open', () => {
+                console.log('Database Connected.....');
+            })
+            // .catch(err => {
+            //     console.log('Connection Failed.');
+            // })
+    }
+};
 
 
-function connectDB() {
-    //Database Connection
-    mongoose.connect(process.env.MONGO_DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: true
-    });
-    const db = mongoose.connection;
 
-    db.on('error', console.error.bind(console, 'Connection Error:'));
-
-    db.once('open', () => {
-            console.log('Database Connected.....');
-        })
-        // .catch(err => {
-        //     console.log('Connection Failed.');
-        // })
-}
-
-module.exports = connectDB;
+// module.exports = connectDB;
